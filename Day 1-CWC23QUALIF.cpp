8954464f03c913471a4382d19094ed22a6f695ec
sonu tomar <145323375+sonutom@users.noreply.github.com>
2024-09-29 20:12:50 +0530

    Add files via upload

---
+++ Day 1-CWC23QUALIF.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+    if (X >= 12) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
