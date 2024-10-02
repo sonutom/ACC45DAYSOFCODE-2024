36d08e1c82209cfe4fc2edbfa40032dea5279ab5
sonu tomar <145323375+sonutom@users.noreply.github.com>
2024-10-02 19:47:38 +0530

    Add files via upload

---
+++ day5-WATERFILLING.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int B1, B2, B3;
+        cin >> B1 >> B2 >> B3;
+        int empty_count = (B1 == 0) + (B2 == 0) + (B3 == 0);
+        if (empty_count >= 2) {
+            cout << "Water filling time" << endl;
+        } else {
+            cout << "Not now" << endl;
+        }
+    }
+
+    return 0;
+}
